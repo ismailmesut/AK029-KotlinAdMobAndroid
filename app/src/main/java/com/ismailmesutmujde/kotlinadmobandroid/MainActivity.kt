fc515ff4b02f34446bcf95cc1de207c963663f47
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-08 17:15:22 +0300

    updated: banner

--- app/src/main/java/com/ismailmesutmujde/kotlinadmobandroid/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlinadmobandroid/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.ismailmesutmujde.kotlinadmobandroid
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.ads.AdListener
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.LoadAdError
 import com.google.android.gms.ads.MobileAds
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -9,10 +14,14 @@ import kotlinx.coroutines.launch
 
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mAdView : AdView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // App Admob ID
         // ca-app-pub-1565056140361297~8243247361
 
         val backgroundScope = CoroutineScope(Dispatchers.IO)
@@ -22,5 +31,47 @@ class MainActivity : AppCompatActivity() {
 
             }
         }
+        mAdView = findViewById(R.id.adView)
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
+
+        mAdView.adListener = object: AdListener() {
+            override fun onAdClicked() {
+                // Code to be executed when the user clicks on an ad.
+            }
+
+            override fun onAdClosed() {
+                // Code to be executed when the user is about to return
+                // to the app after tapping on an ad.
+            }
+
+            override fun onAdFailedToLoad(adError : LoadAdError) {
+                // Code to be executed when an ad request fails.
+            }
+
+            override fun onAdImpression() {
+                // Code to be executed when an impression is recorded
+                // for an ad.
+            }
+
+            override fun onAdLoaded() {
+                // Code to be executed when an ad finishes loading.
+                Toast.makeText(applicationContext,"Ad Loaded", Toast.LENGTH_LONG).show()
+            }
+
+            override fun onAdOpened() {
+                // Code to be executed when an ad opens an overlay that
+                // covers the screen.
+            }
+        }
+
+        // Banner Ad ID
+        // ca-app-pub-1565056140361297/3121425384
+
+        // Banner Test ID
+        // ca-app-pub-3940256099942544/9257395921
+
+        // Interstitial Test ID
+        // ca-app-pub-3940256099942544/1033173712
     }
 }
