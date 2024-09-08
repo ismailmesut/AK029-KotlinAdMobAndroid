7f9d578876a7918a0b020e3b327580703b9e0940
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-08 16:51:58 +0300

    updated: admob sdk

--- app/src/main/java/com/ismailmesutmujde/kotlinadmobandroid/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlinadmobandroid/MainActivity.kt
@@ -2,6 +2,10 @@ package com.ismailmesutmujde.kotlinadmobandroid
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.ads.MobileAds
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +13,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // ca-app-pub-1565056140361297~8243247361
+
+        val backgroundScope = CoroutineScope(Dispatchers.IO)
+        backgroundScope.launch {
+            // Initialize the Google Mobile Ads SDK on a background thread.
+            MobileAds.initialize(this@MainActivity) {
+
+            }
+        }
     }
 }
